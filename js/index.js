682bcddd6b99cd3dcbfcc25135a12f96b6f708ef
shreelaxmikamath <shreelaxmi.k111@gmail.com>
2023-11-17 20:54:44 +0530

    completed updating CSS and HTML.

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function myFunction(x) {
-    if (x.matches) { 
-      document.body.getElementsById('img').src="profile1.png";
-    }
-  }
-  
-  var x = window.matchMedia("(max-width: 800px)")
-  myFunction(x) 
